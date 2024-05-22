9fa66057538c6cc656fa24805932da43d1c38191
Ruel <c0407h@gmail.com>
2024-05-22 17:24:38 +0900

    Publisher를 구독할때, AnyCancellable가 지속적으로 쌓이는 문제 확인 -> cacnel을 통해 이전에 사용한 Publisher를 제거해줌

--- CombineTest/CombineTest/SearchView/SearchViewController.swift
+++ CombineTest/CombineTest/SearchView/SearchViewController.swift
@@ -9,6 +9,126 @@ import UIKit
 import SnapKit
 import Combine
 
+//class SearchViewController: UIViewController {
+//    let viewModel = SearchViewModel()
+//    private var cancellables = Set<AnyCancellable>()
+//    
+//    lazy var refreshControl: UIRefreshControl = {
+//        let rc = UIRefreshControl()
+//        rc.addTarget(self, action: #selector(refresh), for: .valueChanged)
+//        return rc
+//    }()
+//    
+//    lazy var searchTableView: UITableView = {
+//        let tableView = UITableView()
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        tableView.rowHeight = 130
+//        tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: "SearchTableViewCell")
+//        tableView.refreshControl = refreshControl
+//        return tableView
+//    }()
+//    
+//    lazy var searchController: UISearchController = {
+//        let searchController = UISearchController(searchResultsController: nil)
+//        searchController.searchBar.placeholder = "도서명/저자/출판사으로 검색해주세요."
+//        // hidesNavigationBarDuringPresentation -> 검색시 (키보드가 올라왔을때) 네비게이션 타이틀을 가릴지 안가릴지
+//        searchController.hidesNavigationBarDuringPresentation = true
+//        searchController.searchResultsUpdater = self
+//        
+//        return searchController
+//    }()
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setupUI()
+//        setupSearchController()
+//        
+//        viewModel.fetchBookList(searchText: "iOS")
+//        
+//        viewModel.$bookList
+//            .receive(on: DispatchQueue.main)
+//            .sink { [weak self] _ in
+//                self?.searchTableView.reloadData()
+//                self?.refreshControl.endRefreshing()
+//            }
+//            .store(in: &cancellables)
+//        
+//    }
+//    
+//    private func setupSearchController() {
+//        self.navigationItem.searchController = searchController
+//        self.navigationItem.title = "도서 검색"
+//        self.navigationItem.hidesSearchBarWhenScrolling = false
+//    }
+//    
+//    private func setupUI() {
+//        self.view.addSubview(searchTableView)
+//        
+//        searchTableView.snp.makeConstraints {
+//            $0.edges.equalToSuperview()
+//        }
+//    }
+//    
+//    @objc func refresh() {
+//        viewModel.fetchBookList(searchText: "iOS", isRefresh: true)
+//    }
+//    
+//}
+//
+//
+//extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.bookList.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell",
+//                                                    for: indexPath) as? SearchTableViewCell {
+//            cell.bindCell(book: viewModel.bookList[indexPath.row])
+//            
+//            return cell
+//        }
+//        
+//        return UITableViewCell()
+//    }
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let viewModel = BookDetailViewModel(book: viewModel.bookList[indexPath.row])
+//        let vc = BookDetailViewController(viewModel: viewModel)
+//        
+//        self.navigationController?.pushViewController(vc, animated: true)
+//    }
+//    
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let offsetY = scrollView.contentOffset.y
+//        let contentHeight = scrollView.contentSize.height
+//        let height = scrollView.frame.size.height
+//        
+//        if offsetY > contentHeight - height {
+//            let searchText = searchController.searchBar.text == "" ? "iOS" : searchController.searchBar.text
+//            
+//            if viewModel.isFetching != true {
+//                viewModel.fetchBookList(searchText: searchText)
+//            }
+//            
+//        }
+//    }
+//    
+//    
+//}
+//
+//
+//extension SearchViewController: UISearchResultsUpdating {
+//    func updateSearchResults(for searchController: UISearchController) {
+//        let text = searchController.searchBar.text ?? ""
+//        viewModel.fetchBookList(searchText: text == "" ? "iOS" : text.lowercased(), isRefresh: true)
+//    }
+//}
+//
+//
+
+
 class SearchViewController: UIViewController {
     let viewModel = SearchViewModel()
     private var cancellables = Set<AnyCancellable>()
@@ -18,7 +138,7 @@ class SearchViewController: UIViewController {
         rc.addTarget(self, action: #selector(refresh), for: .valueChanged)
         return rc
     }()
-
+    
     lazy var searchTableView: UITableView = {
         let tableView = UITableView()
         tableView.delegate = self
@@ -32,10 +152,8 @@ class SearchViewController: UIViewController {
     lazy var searchController: UISearchController = {
         let searchController = UISearchController(searchResultsController: nil)
         searchController.searchBar.placeholder = "도서명/저자/출판사으로 검색해주세요."
-        // hidesNavigationBarDuringPresentation -> 검색시 (키보드가 올라왔을때) 네비게이션 타이틀을 가릴지 안가릴지
         searchController.hidesNavigationBarDuringPresentation = true
         searchController.searchResultsUpdater = self
-        
         return searchController
     }()
     
@@ -43,16 +161,19 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         setupSearchController()
+        bindViewModel()
         
         viewModel.fetchBookList(searchText: "iOS")
-  
+    }
+    
+    private func bindViewModel() {
         viewModel.$bookList
             .receive(on: DispatchQueue.main)
             .sink { [weak self] _ in
                 self?.searchTableView.reloadData()
+                self?.refreshControl.endRefreshing()
             }
             .store(in: &cancellables)
-
     }
     
     private func setupSearchController() {
@@ -63,7 +184,6 @@ class SearchViewController: UIViewController {
     
     private func setupUI() {
         self.view.addSubview(searchTableView)
-        
         searchTableView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
@@ -72,11 +192,9 @@ class SearchViewController: UIViewController {
     @objc func refresh() {
         viewModel.fetchBookList(searchText: "iOS", isRefresh: true)
     }
-    
 }
 
-
-extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.bookList.count
     }
@@ -85,17 +203,14 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
         if let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell",
                                                     for: indexPath) as? SearchTableViewCell {
             cell.bindCell(book: viewModel.bookList[indexPath.row])
-            
             return cell
         }
-        
         return UITableViewCell()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let viewModel = BookDetailViewModel(book: viewModel.bookList[indexPath.row])
         let vc = BookDetailViewController(viewModel: viewModel)
-        
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
@@ -106,13 +221,12 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
         
         if offsetY > contentHeight - height {
             let searchText = searchController.searchBar.text == "" ? "iOS" : searchController.searchBar.text
-            viewModel.fetchBookList(searchText: searchText)
+            if !viewModel.isFetching {
+                viewModel.fetchBookList(searchText: searchText)
+            }
         }
     }
-    
-    
 }
-
 
 extension SearchViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
@@ -120,5 +234,3 @@ extension SearchViewController: UISearchResultsUpdating {
         viewModel.fetchBookList(searchText: text == "" ? "iOS" : text.lowercased(), isRefresh: true)
     }
 }
-
-
--- CombineTest/CombineTest/SearchView/SearchViewModel.swift
+++ CombineTest/CombineTest/SearchView/SearchViewModel.swift
@@ -8,6 +8,55 @@
 import Foundation
 import Combine
 
+//class SearchViewModel {
+//    let service = SearchService()
+//    
+//    private var pageCnt = 0
+//    private let defaultSearchText = "iOS"
+//    private var cancellables = Set<AnyCancellable>()
+//    
+//    //@Published와 Subject는 같은 역할을 하긴함
+//    //그러나 Subject를 사용할경우 Book 데이터를 append 하지 못함
+//    //Subject를 사용하면 [Book] 타입의 변수를 선언해주고 append 후 send를 해주어야하는 상황이 발생함
+//    //따라서 현상황에서 데이터를 append 할 수 있는 @Published를 사용하였음
+//    @Published var bookList: [Book] = []
+//    //    var booksList = CurrentValueSubject<[Book], Never>([])
+//    //    var booksList = PassthroughSubject<[Book], Never>()
+//    var isFetching = false
+//    
+//    //MARK: - Combine을 사용하여 API호출
+//    func fetchBookList(searchText: String? = nil, isRefresh: Bool = false) {
+//        isFetching = true
+//        
+//        if isRefresh {
+//            pageCnt = 1
+//        } else {
+//            pageCnt += 1
+//        }
+//        
+//        let query = searchText ?? defaultSearchText
+//        
+//        service.fetchBooks(searchText: query, page: pageCnt)
+//            .sink { [weak self] completion in
+//                guard case .failure(_) = completion else { return }
+//                self?.isFetching = false
+//                
+//            } receiveValue: { [weak self] books in
+//                guard let self = self else { return }
+//                self.isFetching = false
+//                if self.pageCnt == 1 {
+//                    self.bookList = books
+//                } else {
+//                    if !bookList.isEmpty {
+//                        self.bookList.append(contentsOf: books)
+//                    }
+//                }
+//            }
+//            .store(in: &cancellables)
+//    }
+//    
+//}
+
 class SearchViewModel {
     let service = SearchService()
     
@@ -15,16 +64,9 @@ class SearchViewModel {
     private let defaultSearchText = "iOS"
     private var cancellables = Set<AnyCancellable>()
     
-    //@Published와 Subject는 같은 역할을 하긴함
-    //그러나 Subject를 사용할경우 Book 데이터를 append 하지 못함
-    //Subject를 사용하면 [Book] 타입의 변수를 선언해주고 append 후 send를 해주어야하는 상황이 발생함
-    //따라서 현상황에서 데이터를 append 할 수 있는 @Published를 사용하였음
     @Published var bookList: [Book] = []
-    //    var booksList = CurrentValueSubject<[Book], Never>([])
-    //    var booksList = PassthroughSubject<[Book], Never>()
     var isFetching = false
     
-    //MARK: - Combine을 사용하여 API호출
     func fetchBookList(searchText: String? = nil, isRefresh: Bool = false) {
         isFetching = true
         
@@ -36,10 +78,15 @@ class SearchViewModel {
         
         let query = searchText ?? defaultSearchText
         
+        // 기존 구독을 취소
+        cancellables.forEach { $0.cancel() }
+        cancellables.removeAll()
+        
         service.fetchBooks(searchText: query, page: pageCnt)
             .sink { [weak self] completion in
-                guard case .failure(_) = completion else { return }
+                guard case .failure(let error) = completion else { return }
                 self?.isFetching = false
+                print("Error fetching books: \(error)")
                 
             } receiveValue: { [weak self] books in
                 guard let self = self else { return }
@@ -47,12 +94,11 @@ class SearchViewModel {
                 if self.pageCnt == 1 {
                     self.bookList = books
                 } else {
-                    if !bookList.isEmpty {
+                    if !books.isEmpty {
                         self.bookList.append(contentsOf: books)
                     }
                 }
             }
             .store(in: &cancellables)
     }
-    
 }
