5a17783deea61a14b31c7dc8f43030cc186ab193
Ruel <c0407h@gmail.com>
2024-05-23 11:10:33 +0900

    💫 Add Combine_Timer

--- CombineExample_Playground/Combine_Notification.playground/Contents.swift
+++ CombineExample_Playground/Combine_Notification.playground/Contents.swift
@@ -21,11 +21,14 @@ let eventPublisher = NotificationCenter
     }
 
 let theEventTitleLabel = UILabel()
+
+/*
 let newEventLabelSubscriber = Subscribers.Assign(object: theEventTitleLabel, keyPath: \.text)
-
-
 eventPublisher
     .subscribe(newEventLabelSubscriber)
+*/
+// 위와 같이 사용할 수 있고, 아래와 같이 사용 가능
+eventPublisher.assign(to: \.text, on: theEventTitleLabel)
 
 let event = Event(title: "Introduction to Combine Framework", scheduledOn: Date())
 NotificationCenter.default.post(name: .newEvent, object: event)
